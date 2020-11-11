57bf9d159e446d9e5540e7a9b543c973858f33c2
mikkelhartung <mikkelhrasmussen@live.dk>
2020-11-11 18:30:29 +0100

    Add websocket setup for subscriptions

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './assets/stylesheets/global.scss'
-import { Auth0Provider } from "@auth0/auth0-react";
-import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from '@auth0/auth0-react'
+import reportWebVitals from './reportWebVitals'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { BrowserRouter as Router } from 'react-router-dom'
-import Routes from "./routes";
+import Routes from './routes'
+import { split, HttpLink } from '@apollo/client'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { WebSocketLink } from '@apollo/client/link/ws'
 
-export const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  cache: new InMemoryCache(),
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
 })
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+  },
+})
+
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  httpLink
+)
+
+export const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
@@ -27,9 +57,9 @@ ReactDOM.render(
       </Router>
     </Auth0Provider>
   </ApolloProvider>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,25 +1,38 @@
 import React, { FC } from 'react'
 import Layout from '../../components/Layout'
-import { PomodoroStatus, useGetPomodorosSubscription, useUpdatePomodoroMutation } from '../../graphql/generated/graphql'
+import {
+  PomodoroStatus,
+  useGetPomodorosSubscription,
+  useUpdatePomodoroMutation,
+} from '../../graphql/generated/graphql'
 
 const Dashboard: FC = () => {
-
-  // const { data } = useGetUsersQuery()
   const { data } = useGetPomodorosSubscription()
-  const [updatePomodoro] = useUpdatePomodoroMutation({
+  const [startPomodoro] = useUpdatePomodoroMutation({
     variables: {
       email: 'itsmikkelhartung@gmail.com',
-      status: PomodoroStatus.Started
-    }
+      status: PomodoroStatus.Started,
+    },
+  })
+  const [stopPomodoro] = useUpdatePomodoroMutation({
+    variables: {
+      email: 'itsmikkelhartung@gmail.com',
+      status: PomodoroStatus.Stopped,
+    },
+  })
+  const [pausePomodoro] = useUpdatePomodoroMutation({
+    variables: {
+      email: 'itsmikkelhartung@gmail.com',
+      status: PomodoroStatus.Pause,
+    },
   })
 
-  console.log(data)
   return (
     <Layout>
       Dashboard
-
-      <button onClick={() => updatePomodoro()}>Update</button>
-
+      <button onClick={() => startPomodoro()}>Start</button>
+      <button onClick={() => stopPomodoro()}>Stop</button>
+      <button onClick={() => pausePomodoro()}>Pause</button>
       <h3>Users:</h3>
       {data?.pomodoroStarted?.email}
       {data?.pomodoroStarted?.status}
